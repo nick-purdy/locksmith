515aa1355a054ba4617b5be0351f0913f218f588
Nick Purdy <nick-purdy@users.noreply.github.com>
2017-03-16 16:49:32 +1100

    Worked on edit page

--- secret-edit.js
+++ secret-edit.js
@@ -1,11 +1,85 @@
 class SecretEdit extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {};
+    }
+
+    componentDidMount() {
+        this.viewSecret()
+    }
+
+    viewSecret() {
+        const path = "/v1/" + this.props.path
+
+        $.ajax({
+            url: path,
+            context: this,
+            headers: {
+                "X-Vault-Token": globalLoginToken
+            },
+            type: 'GET',
+            success: function(result) {
+                console.info(result)
+                this.setState(result)
+            },
+            error: function(e) {
+                console.log(e);
+                console.log(e.responseJSON.errors);
+                this.setState({errors: e.responseJSON.errors});
+            },
+        });
+    }
+
+    handleAddNewSecretRow() {
+        alert("hello")
     }
 
     render() {
+        let form = (<div>Loading...</div>)
+        let rows = []
+
+        if (this.state.data) {
+
+            console.log(this.state.data)
+            for (var propertyName in this.state.data) {
+                if (this.state.data.hasOwnProperty(propertyName)) {
+                    rows.push(
+                        <SecretEditRow key={propertyName} name={propertyName} secret={this.state.data[propertyName]} />
+                    )
+                }
+            }
+
+            form = (
+                <form>
+                    <fieldset>
+                        <div className="container">
+                            {rows}
+                            <p><a onClick={this.handleAddNewSecretRow}>Add New</a></p>
+                        </div>
+                        <input className="button-primary" value="Save" type="submit" />
+                    </fieldset>
+                </form>
+            )
+        }
+
         return (
-            <div></div>
+            <section className="container" id="secrets">
+                <h5 className="title">Edit Secret</h5>
+                <p><code>{this.props.path}</code></p>
+                {form}
+            </section>
+        )
+    }
+}
+
+class SecretEditRow extends React.Component {
+    render() {
+        return (
+            <div className="row">
+                <div className="column"><input placeholder="Secret Name" value={this.props.name} type="text" /></div>
+                <div className="column"><input placeholder="Secret Value" value={this.props.secret} type="text" /></div>
+                <div className="column"><a>remove</a></div>
+            </div>
         )
     }
 }
--- secret.js
+++ secret.js
@@ -1,94 +1,32 @@
 
 class Secret extends React.Component {
-
     constructor(props) {
         super(props)
 
         this.state = {
-            currentFolder: ["secret"]
+            view: "list",
+            path: "secret"
         };
+
+        this.handleEditSecret = this.handleEditSecret.bind(this)
     }
 
-    componentDidMount() {
-        this.listSecrets()
-    }
-
-    listSecrets() {
-        const path = "/v1/" + this.state.currentFolder.join("/") + "/"
-
-        $.ajax({
-            url: path,
-            context: this,
-            headers: {
-                "X-Vault-Token": globalLoginToken
-            },
-            type: 'LIST',
-            success: function(result) {
-                console.info(result)
-                this.setState(result)
-            },
-            error: function(e) {
-                console.log(e);
-                console.log(e.responseJSON.errors);
-                this.setState({errors: e.responseJSON.errors});
-            },
-        });
-    }
-
-    handleSecretExpansion(path, event) {
-        let newPath = this.state.currentFolder
-        newPath.push(path.replace("/", ""))
-        this.setState({currentFolder: newPath})
-        this.listSecrets()
-    }
-
-
-    handleSecretEdit(path, event) {
-        alert("Editing: " + path + ", " + this.state.currentFolder)
-
-    }
-
-
-    handleBreadCrumb(path) {
-        this.setState({currentFolder: path})
-        this.listSecrets()
+    handleEditSecret(path) {
+        this.setState({
+            view: "edit",
+            path: "secret/hello/world"
+        })
     }
 
     render() {
-        let secrets = []
-
-        if (this.state && this.state.data && this.state.data.keys) {
-            for (let index in this.state.data.keys) {
-                const path = this.state.data.keys[index]
-                secrets.push(
-                            <SecretsRow key={path} path={path} onClick={this.handleSecretExpansion.bind(this, path)} onEditClick={this.handleSecretEdit.bind(this, path)} />
-                            )
-            }
+        if (this.state.view === "list") {
+            return (
+                <SecretList path={this.state.path} onEditSecret={this.handleEditSecret} />
+            )
         } else {
-            secrets.push(
-                        <tr>
-                            <td>Loading...</td>
-                            <td><a>view</a> &nbsp; <a>edit</a> &nbsp; <a>delete</a></td>
-                        </tr>
-                        )
+            return (
+                <SecretEdit path={this.state.path} onEditSecret={this.handleEditSecret} />
+            )
         }
-
-        return (
-            <section className="container" id="secrets">
-                <h5 className="title">Secrets</h5>
-                <SecretsBreadCrumb currentFolder={this.state.currentFolder} onClick={this.handleBreadCrumb.bind(this)} />
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Secret Path</th>
-                            <th>Tools</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {secrets}
-                    </tbody>
-                </table>
-            </section>
-        )
     }
 }
