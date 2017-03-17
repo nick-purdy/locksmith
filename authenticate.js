6833942d25d807cc27afc1be84f6e34fc29fbc4d
Nick Purdy <nick-purdy@users.noreply.github.com>
2017-03-17 13:36:27 +1100

    Made success message when you enter token

--- authenticate.js
+++ authenticate.js
@@ -3,7 +3,8 @@ class Authenticate extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            token: ""
+            token: "",
+            success: ""
         };
 
         this.handleChange = this.handleChange.bind(this);
@@ -17,18 +18,32 @@ class Authenticate extends React.Component {
     handleSubmit(e) {
         e.preventDefault();
         globalLoginToken = this.state.token
+        this.state.success = "Successfully logged in"
+        this.forceUpdate()
     }
 
     render() {
+
+        let content = (
+            <form onSubmit={this.handleSubmit}>
+                <label>Token</label>
+                <input value={this.state.token} onChange={this.handleChange} placeholder="Your token. Input will be hidden..." id="tokenField" type="password" />
+                <input className="button-primary" value="Login" type="submit" />
+            </form>
+        )
+
+        if (this.state.success) {
+            content = (
+                <div className="success">
+                    {this.state.success}
+                </div>
+            )
+        }
+
         return (
             <section className="container" id="authenticate">
                 <h5 className="title">Authenticate</h5>
-
-                <form onSubmit={this.handleSubmit}>
-                    <label>Token</label>
-                    <input value={this.state.token} onChange={this.handleChange} placeholder="Your token. Input will be hidden..." id="tokenField" type="password" />
-                    <input className="button-primary" value="Login" type="submit" />
-                </form>
+                {content}
             </section>
         )
     }
